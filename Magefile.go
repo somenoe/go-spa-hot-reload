a5fd17d7b006ff30606a86a527201c08aeb9537d
somenoe <otashi24@gmail.com>
2025-01-08 00:02:59 +0700

    feat: Add wasm, build, run, and dev mage targets

--- Magefile.go
+++ Magefile.go
@@ -3,17 +3,49 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 )
 
-// Runs go mod download and then installs the binary.
-func Build() error {
-	if err := sh.RunWith(map[string]string{
+const (
+	DIFF_FORMATTER = "05.000"
+)
+
+func Wasm() {
+	startTime := time.Now()
+	sh.RunWith(map[string]string{
 		"GOARCH": "wasm",
 		"GOOS":   "js",
-	}, "go", "build", "-o", "web/app.wasm"); err != nil {
-		return err
-	}
+	}, "go", "build", "-o", "web/app.wasm")
+	endTime := time.Now()
+	diff := endTime.Sub(startTime)
+	formattedDiff := time.Time{}.Add(diff).Format(DIFF_FORMATTER)
 
-	return sh.Run("go", "build", "-o", "tmp/main.exe")
+	fmt.Println("wasm\t=>", formattedDiff)
+}
+
+func Build() {
+	mg.Deps(Wasm)
+	startTime := time.Now()
+	sh.Run("go", "build", "-o", "tmp/main.exe")
+	endTime := time.Now()
+	diff := endTime.Sub(startTime)
+	formattedDiff := time.Time{}.Add(diff).Format(DIFF_FORMATTER)
+
+	fmt.Println("build\t=>", formattedDiff)
+}
+
+func Run() {
+	mg.Deps(Build)
+
+	sh.Run("tmp/main.exe")
+}
+
+func Dev() {
+	mg.Deps(Wasm)
+
+	sh.Run("go", "run", ".")
 }
